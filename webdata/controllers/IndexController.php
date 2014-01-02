0116080c89bd0c009efe7e2342cc8eff7be8d748
Ronny Wang <ronnywang@gmail.com>
2014-01-02 17:01:05 +0800

    加上首頁功能

---
+++ webdata/controllers/IndexController.php
@@ -0,0 +1,44 @@
+<?php
+
+class IndexController extends Pix_Controller
+{
+    public function dataAction()
+    {
+        list(, /*index*/, /*data*/, $id) = explode('/', $this->getURI());
+        if (!$dataset = DataSet::find(intval($id))) {
+            return $this->redirect('/');
+        }
+
+        $this->view->dataset = json_decode($dataset->data);
+        $this->view->types = $this->view->dataset->types;
+        $this->view->name = $dataset->name;
+        $this->view->title = $dataset->name . ' :: ';
+    }
+
+    public function listAction()
+    {
+        if ($_GET['type'] == 'search') {
+            $this->view->datasets = DataSet::search("POSITION('" . addslashes($_GET['q']) . "' IN CONCAT(data->>'name', data->>'dataDesc', data->>'columnDesc', data->>'keyword')) > 0");
+            $this->view->desc = '搜尋「' . strval($_GET['q']) . '」結果';
+            return;
+        }
+
+        $support_query = array(
+            'dataTypeName' => '資料集類型',
+            'orgFullname' => '資料集提供機關',
+        );
+        foreach ($support_query as $type => $type_name) {
+            if (array_key_exists($type, $_GET)) {
+                $this->view->datasets = DataSet::search("data->>'{$type}' = '" . addslashes($_GET[$type]) . "'");
+                $this->view->desc = $type_name . '：' . $_GET[$type];
+                return;
+            }
+        }
+        $this->view->datasets = DataSet::search(1);
+        $this->view->desc = '全部資料集';
+    }
+
+    public function indexAction()
+    {
+    }
+}
