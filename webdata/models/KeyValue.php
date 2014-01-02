7f89e34056b63d648d7b922725b191c38bd4dee0
Ronny Wang <ronnywang@gmail.com>
2014-01-02 17:33:24 +0800

    加上 KeyValue::set() ::get()

--- webdata/models/KeyValue.php
+++ webdata/models/KeyValue.php
@@ -10,4 +10,23 @@ class KeyValue extends Pix_Table
         $this->_columns['key'] = array('type' => 'varchar', 'size' => 32);
         $this->_columns['value'] = array('type' => 'text');
     }
+
+    public function set($key, $value)
+    {
+        try {
+            KeyValue::insert(Array(
+                'key' => $key,
+                'value' => $value,
+            ));
+        } catch (Pix_Table_DuplicateException $e) {
+            KeyValue::find($key)->update(array(
+                'value' => $value,
+            ));
+        }
+    }
+
+    public function get($key)
+    {
+        return KeyValue::find($key)->value;
+    }
 }
